1edffda0f970d48117d14894ea38fe8cd1862219
Ken Hibino <ken.hibino7@gmail.com>
2017-02-04 16:09:19 -0800

    Updates

--- demo/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Demo from './Demo'
-
-ReactDOM.render(
-  React.createElement(Demo, {}),
-  document.getElementById('app')
-)
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import PlacesAutocomplete from './PlacesAutocomplete'
-import { geocodeByAddress } from './utils'
-
-export {
-  geocodeByAddress
-}
-
-export default PlacesAutocomplete
